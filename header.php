b849df8bd89f9f208373353e03b41776345967f3
Ollie Robinson <ollie@ollierobinson.co.uk>
2021-02-09 15:33:44 +0000

    Database support for index.php

--- header.php
+++ header.php
@@ -6,15 +6,15 @@
     </div>
     <div id="headerright">
         <?php
-            $conn = require("database.php"); // Connect to the database.
-            $result = $conn -> query("SELECT ID, title, href FROM links"); // Query the links section from the database
-            while ($row = $result -> fetch_assoc()) {
-                $className = "menu"; // The default class name is "menu"
-                if ($_SERVER['REQUEST_URI'] == $row['href']) {
-                    $className = "menu-highlighted"; // However if the current URI is equal to the href of the row, highlight the link (to make it look selected).
-                }
-                echo "<a class=\"$className\" href=\"".$row['href']."\">".$row['title']."</a>"; // Echo the link.
+        $conn = require("database.php"); // Connect to the database.
+        $result = $conn -> query("SELECT ID, title, href FROM links"); // Query the links section from the database
+        while ($row = $result -> fetch_assoc()) {
+            $className = "menu"; // The default class name is "menu"
+            if ($_SERVER['REQUEST_URI'] == $row['href']) {
+                $className = "menu-highlighted"; // However if the current URI is equal to the href of the row, highlight the link (to make it look selected).
             }
+            echo "<a class=\"$className\" href=\"".$row['href']."\">".$row['title']."</a>"; // Echo the link.
+        }
         ?>
     </div> <!-- PHP generated stuff -->
 </div>
